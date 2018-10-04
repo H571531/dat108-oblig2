6a2e52ccde62fa9f38a6b1f0d3bcc9753fd32ece
tohov <tohov@DESKTOP-A7L6L26>
2018-10-04 17:35:34 +0200

    Kristoffer-utgave 4.10

---
+++ Oblig2/src/no/hvl/dat108/oblig2/kristoffer/Vare.java
@@ -0,0 +1,42 @@
+package no.hvl.dat108.oblig2.kristoffer;
+
+public class Vare {
+	
+	String navn;
+	//statisk id for å gjøre sletting lettere?
+	
+	public Vare(String navn) {
+		this.navn = navn;
+	}
+	
+	public String getNavn() {
+		return navn;
+	}
+	
+	@Override
+	public String toString() {
+		return navn;
+	}
+
+	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vare other = (Vare) obj;
+		if (navn == null) {
+			if (other.navn != null)
+				return false;
+		} else if (!navn.equals(other.navn))
+			return false;
+		return true;
+	}
+	
+	
+
+}
